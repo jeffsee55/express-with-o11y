8dc512d5d67d9a21ed07746c556043ce2f8ad117
Jeff See <jeffsee.55@gmail.com>
2025-10-28 08:55:54 -0700

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("static"));
 
-// Home route - HTML
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!");
 });
 
 app.get("/about", function (req, res) {
